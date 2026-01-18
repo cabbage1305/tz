76d477c69f9d4420890a54b06a0e84d19967943d
cabbage1305 <cabbage1305@mail.ru>
2026-01-18 22:11:14 +0300

    connecting to database, updating CRUD for Articles

--- back/models/artModel.js
+++ back/models/artModel.js
@@ -1,16 +1,44 @@
+const Sequelize = require("sequelize");
+const sequelize = require("../db.js");
+
 const Article = sequelize.define("article", {
   id: {
     type: Sequelize.INTEGER,
     autoIncrement: true,
     primaryKey: true,
-    allowNull: false
+    allowNull: false,
   },
   name: {
     type: Sequelize.STRING,
-    allowNull: false
+    allowNull: false,
   },
   content: {
     type: Sequelize.TEXT,
-    allowNull: false
-  }
-})
+    allowNull: false,
+  },
+});
+
+const Comments = sequelize.define("comments", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false,
+  },
+  text: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+});
+
+Article.hasMany(Comments);
+
+sequelize
+  .sync()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => console.log(err));
+
+module.exports = Article;
+module.exports = Comments;
